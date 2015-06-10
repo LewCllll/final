44568fb88cb2eff0eb09c879f0990f04c9896aeb
JoshuaChang21 <joshua0418@gmail.com>
2015-06-10 14:37:48 +0800

    0610 version2

--- fb.js
+++ fb.js
@@ -1,22 +1,3 @@
- //fbapi
- window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '836005236481666',
-      xfbml      : true,
-      version    : 'v2.3'
-    });
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
-
-
   // This is called with the results from from FB.getLoginStatus().
   function statusChangeCallback(response) {
     console.log('statusChangeCallback');
